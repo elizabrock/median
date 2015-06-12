07334e22a4c530af291ba35249ecb3b029e92797
Margaret Clinard <margaret.clinard@gmail.com>
2015-06-12 10:47:56 -0500

    Updated validations on twitter_id, twitter_nickname, oauth_token, oauth_secret. Corrected spacing on update with twitter method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,17 @@ class User < ActiveRecord::Base
   validates :email, format: { with: /.+@.+\..+/, message: "must be an email address" }, uniqueness: true
   validates :password, confirmation: true
   validates :password, :password_confirmation, presence: { on: :create }
-  validates_uniqueness_of :twitter_id
-  validates_uniqueness_of :twitter_nickname
-  validates_uniqueness_of :oauth_token
-  validates_uniqueness_of :oauth_secret
+  validates_uniqueness_of :twitter_id, allow_blank: true
+  validates_uniqueness_of :twitter_nickname, allow_blank: true
+  validates_uniqueness_of :oauth_token, allow_blank: true
+  validates_uniqueness_of :oauth_secret, allow_blank: true
 
   def update_with_twitter_oauth(auth)
-      self.twitter_id = auth.uid
-      self.twitter_nickname = "@#{auth.info.nickname}"
-      self.oauth_token = auth.credentials.token
-      self.oauth_secret = auth.credentials.secret
-      self.save
+    self.twitter_id = auth.uid
+    self.twitter_nickname = "@#{auth.info.nickname}"
+    self.oauth_token = auth.credentials.token
+    self.oauth_secret = auth.credentials.secret
+    self.save
   end
 
 end
