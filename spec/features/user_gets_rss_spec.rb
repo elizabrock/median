3d7d9428d887a8516637c29a06cef2484707912a
thedean11 <edean1010@gmail.com>
2015-06-12 10:19:07 -0500

    added more robust rss tests, added sitemap test

--- spec/features/user_gets_rss_spec.rb
+++ spec/features/user_gets_rss_spec.rb
@@ -2,27 +2,34 @@ feature "User gets RSS Feed" do
 
   let!(:user){ Fabricate(:user) }
   let!(:foo_post){ Fabricate(:post, author: user, title: "Foo", body: "Bar") }
+  let!(:user2){ Fabricate(:user) }
+  let!(:bar_post){ Fabricate(:post, author: user2, title: "Foo", body: "Bar") }
 
   scenario "GET Full RSS Feed" do
     visit root_path
     click_link "rss"
-    page.has_content?('rss')
-    page.has_content?('channel')
-    page.has_content?('title')
-    page.has_content?('author')
-    page.has_content?('description')
-    page.has_content?('pubDate')
+    expect(page.source).to include('rss')
+    expect(page.source).to include('channel')
+    expect(page.source).to include('title')
+    expect(page.source).to include('author')
+    expect(page.source).to include('description')
+    expect(page.source).to include('pubDate')
+    expect(page.source).to include("#{user.name}")
+    expect(page.source).to include("#{foo_post.title}")
+    expect(page.source).to include("#{user2.name}")
+    expect(page.source).to include("#{bar_post.title}")
   end
 
   scenario "GET one Users RSS Feed" do
     visit user_posts_path(user)
     click_link "rss"
-    page.has_content?('rss')
-    page.has_content?('channel')
-    page.has_content?('<title>#{user.name}</title>')
-    page.should_not(have_content('author'))
-    page.has_content?('description')
-    page.has_content?('pubDate')
+    expect(page.source).to include('rss')
+    expect(page.source).to include('channel')
+    expect(page.source).to include("<title>#{user.name}</title>")
+    expect(page.source).to_not(include('author'))
+    expect(page.source).to include('description')
+    expect(page.source).to include('pubDate')
+    expect(page.source).to include("#{foo_post.title}")
   end
 
 end
